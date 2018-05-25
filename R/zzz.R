ebc4e9ff2e2616faf607dc3301fd8befc68c5b71
Renato <renatussouza7@gmail.com>
2018-05-25 13:52:00 -0300

    Texto de citação

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onLoad <- function(libname, pkgname){
-    packageStartupMessage("Bla bla bla\nteste para ver edição\nteste do \n")
+    packageStartupMessage("All data are obtained from the Brazilian Center for Research in Financial Economics of the University of São Paulo (NEFIN) website – Núcleo de Pesquisas em Economia Financeira da Universidade de São Paulo <http://nefin.com.br>.\n R package version 1.1.0. https://github.com/renefin/nefin")
 }
